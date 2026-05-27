63a5bd58102b39ac07cfd0de19991bf6500584a3
HarikaPatro <patroharika866@gmail.com>
2026-05-27 16:17:23 +0530

    for-loop

---
+++ for-loop/variables.tf
@@ -0,0 +1,17 @@
+variable "instances" {
+    #default = [ "mongodb", "redis", "mysql", "rabbitmq", "catalogue", "user", "cart", "shipping", "payment", "frontend" ]
+    default = [ "mongodb", "redis"]
+    # default = {
+    #     mongodb = "t3.micro"
+    #     redis = "t3.micro"
+    #     mysql = "t3.small"
+    # }
+}
+
+variable "zone-id" {
+    default = "Z00144233DSWXIB51KLPM"
+}
+
+variable "domain_name" {
+    default = "daws87s.fun"
+}
